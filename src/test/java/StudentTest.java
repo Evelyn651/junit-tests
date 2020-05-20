63ab8febd7a795cae4d6db6cffc7838f8b248f99
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-20 14:58:57 -0500

    feat: adding some tests to see if methods will work

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    Student evey;
+    Student severalGrades;
+
+    @Before
+    public void init(){
+        evey = new Student(1, "Evelyn A");
+        evey.addGrade(100);
+        severalGrades = new Student(2, "Amber V");
+        severalGrades.addGrade(95);
+        severalGrades.addGrade(85);
+        severalGrades.addGrade(95);
+    }
+
+    @Test
+    public void ifStudentCanBeCreated(){
+        assertEquals("Evelyn A", evey.getName());
+        assertEquals(1, evey.getId());
+    }
+
+    @Test
+    public void testIfGradesWillBeAdded(){
+        assertSame(90, evey.getGrades().get(0));
+    }
+
+
 }
