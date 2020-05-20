02a1559c1d5ef39d78e39441999695a31fd02fca
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-20 12:12:28 -0500

    feat: added student getName getId addGrade methods

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -4,4 +4,24 @@ public class Student {
     ArrayList<Integer> grades;
     private String name;
     private long id;
+
+    public Student(long id, String name){
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+
 }
